8dbbb2e4066d526ac128820364de83af029157ad
xRuiAlves <ruialves.esrt.98@gmail.com>
2023-12-03 08:02:55 +0000

    Add day 3 part 2

--- Day3.scala
+++ Day3.scala
@@ -3,13 +3,20 @@
 //> using file util/ResourceUtils.scala
 //> using resourceDir inputs
 
+import scala.collection.mutable
 import util.ResourceUtils.readResourceLines
 
 object Day3 {
+  type Coord2D = (Int, Int)
+
+  final case class Number(value: Int, start: Coord2D)
+
   final val EMPTY_SPACE = '.'
+  final val GEAR = '*'
 
   def main(args: Array[String]): Unit = {
     val matrix = readResourceLines("day3.txt").map(_.toCharArray)
+    val numbers = mutable.Map[Coord2D, Number]()
 
     def isInBounds(i: Int, j: Int): Boolean = {
       i >= 0 && j >= 0 && i < matrix.length && j < matrix(i).length
@@ -26,7 +33,7 @@ object Day3 {
 
     def parseNumber(i: Int, j: Int): Int = matrix(i).drop(j).takeWhile(_.isDigit).mkString.toInt
 
-    def getNeighbours(i: Int, j: Int): Array[(Int, Int)] = Array(
+    def getNeighbours(i: Int, j: Int): Array[Coord2D] = Array(
       (i - 1, j - 1),
       (i - 1, j),
       (i - 1, j + 1),
@@ -45,17 +52,36 @@ object Day3 {
     }
 
     def getPartNumber(i: Int, j: Int): Int = if (!getValue(i, j).isDigit) 0 else {
-      val isPartNumber = matrix(i).zipWithIndex.drop(j).takeWhile(_._1.isDigit).map(_._2).exists {
-        case digitJ => hasSymbolNeighbor(i, digitJ)
-      }
+      val isPartNumber = matrix(i).zipWithIndex.drop(j).takeWhile(_._1.isDigit).map(_._2).exists(digitJ =>
+        hasSymbolNeighbor(i, digitJ)
+      )
 
       val partNumber = if (isPartNumber) parseNumber(i, j) else 0
+
+      if (isPartNumber) {
+        matrix(i).zipWithIndex.drop(j).takeWhile(_._1.isDigit).map(_._2).foreach(digitJ =>
+          numbers((i, digitJ)) = Number(partNumber, (i, j))
+        )
+      }
+
       floodFillDigitsToEmpty(i, j)
       partNumber
     }
 
-    var part1 = 0
+    def getGearValue(i: Int, j: Int): Long = if (getValue(i, j) != GEAR) 0 else {
+      val neighborNumbers = getNeighbours(i, j)
+        .map {
+          case (neighborI, neighborJ) => numbers.get((neighborI, neighborJ))
+        }
+        .filter(_.isDefined)
+        .map(_.get)
+        .distinct
 
+      if (neighborNumbers.length == 2) neighborNumbers.map(_.value).product
+      else 0
+    }
+
+    var part1 = 0
     for (
       i <- matrix.indices;
       j <- matrix(i).indices
@@ -63,7 +89,15 @@ object Day3 {
       part1 += getPartNumber(i, j)
     }
 
-    println(s"Part 1: $part1")
-  }
+    var part2 = 0L
+    for (
+      i <- matrix.indices;
+      j <- matrix(i).indices
+    ) {
+      part2 += getGearValue(i, j)
+    }
 
+    println(s"Part 1: $part1")
+    println(s"Part 2: $part2")
+  }
 }
