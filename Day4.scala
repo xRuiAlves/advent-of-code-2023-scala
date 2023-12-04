d1ea4f8b8d46f416d57487b042eec4606c158376
xRuiAlves <ruialves.esrt.98@gmail.com>
2023-12-04 08:10:38 +0000

    Add day 4 part 2

--- Day4.scala
+++ Day4.scala
@@ -11,15 +11,16 @@ import scala.collection.mutable
 object Day4 {
 
   final case class Card(private val cardStr: String) {
-    final val value = {
+    final val numMatchingNumbers = {
       val numberGroups = cardStr.split(": ").last.split(" \\| ")
       val winningNumbers = parseNumbersStr(numberGroups.head)
       val myNumbers = parseNumbersStr(numberGroups.last)
-      val myWinningNumbers = myNumbers.count(num => winningNumbers.contains(num))
-
-      if (myWinningNumbers == 0) 0
-      else 1 << (myWinningNumbers - 1)
+      myNumbers.count(num => winningNumbers.contains(num))
     }
+
+    final val value =
+      if (numMatchingNumbers == 0) 0
+      else 1 << (numMatchingNumbers - 1)
   }
   object Card {
     private final def parseNumbersStr(numbersStr: String): Set[Int] = numbersStr
@@ -32,9 +33,17 @@ object Day4 {
   def main(args: Array[String]): Unit = {
     val input = readResourceLines("day4.txt")
     val cards = input.map(cardStr => Card(cardStr))
+    val cardCounts = cards.map(_ => 0)
+
+    def visit(cardId: Int): Unit = if (cardId < cards.length) {
+      cardCounts(cardId) += 1
+      for (i <- 0 until cards(cardId).numMatchingNumbers) visit(cardId + i + 1)
+    }
+
+    cards.indices.foreach(visit)
 
     val part1 = cards.map(_.value).sum
-    val part2 = 0
+    val part2 = cardCounts.sum
 
     println(s"Part 1: $part1")
     println(s"Part 2: $part2")
