fd68d4d9244e8bd545cbf2b878fdce1106ff620e
xRuiAlves <ruialves.esrt.98@gmail.com>
2023-12-06 07:33:28 +0000

    Refactor day 6

--- Day6.scala
+++ Day6.scala
@@ -12,24 +12,29 @@ object Day6 {
   def main(args: Array[String]): Unit = {
     val input = readResourceLines("day6.txt")
 
-    val times = input.head.split(" ").filter(_.nonEmpty).drop(1).map(_.toLong)
-    val distances = input.last.split(" ").filter(_.nonEmpty).drop(1).map(_.toLong)
+    val times = parseLineNumbers(input.head)
+    val distances = parseLineNumbers(input.last)
 
-    val timeTotalRace = input.head.stripPrefix("Time: ").replace(" ", "").toLong
-    val distanceTotalRace = input.last.stripPrefix("Distance: ").replace(" ", "").toLong
+    val timeTotalRace = parseLineNumber(input.head, "Time: ")
+    val distanceTotalRace = parseLineNumber(input.last, "Distance: ")
 
-    val part1 = times.zip(distances).map {
-      case (time, distanceToBeat) => numWaysToBeatRace(time, distanceToBeat)
-    }.product
+    val part1 = times
+      .zip(distances)
+      .map {
+        case (time, distanceToBeat) => numWaysToBeatRace(time, distanceToBeat)
+      }.product
     val part2 = numWaysToBeatRace(timeTotalRace, distanceTotalRace)
 
     println(s"Part 1: $part1")
     println(s"Part 2: $part2")
   }
 
+  def parseLineNumbers(line: String): Array[Long] = line.split(" ").filter(_.nonEmpty).drop(1).map(_.toLong)
+
+  def parseLineNumber(line: String, prefix: String): Long = line.stripPrefix(prefix).replace(" ", "").toLong
+
   @tailrec
-  def numWaysToBeatRace(raceTime: Long, distanceToBeat: Long, initialHold: Long = 1L, numWaysAcc: Long = 0L): Long = if (initialHold == raceTime) numWaysAcc else {
-    val myDistance = (raceTime - initialHold) * initialHold
-    numWaysToBeatRace(raceTime, distanceToBeat, initialHold + 1, numWaysAcc + (if (myDistance > distanceToBeat) 1L else 0L))
-  }
+  def numWaysToBeatRace(raceTime: Long, distanceToBeat: Long, initialHold: Long = 1L, numWaysAcc: Long = 0L): Long =
+    if (initialHold == raceTime) numWaysAcc
+    else numWaysToBeatRace(raceTime, distanceToBeat, initialHold + 1, numWaysAcc + (if ((raceTime - initialHold) * initialHold > distanceToBeat) 1L else 0L))
 }
