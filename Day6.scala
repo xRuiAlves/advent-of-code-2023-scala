b906cae28ad1de94c81a73e0412ca7c62e00095d
xRuiAlves <ruialves.esrt.98@gmail.com>
2023-12-06 07:29:16 +0000

    Add day 6 part 2

--- Day6.scala
+++ Day6.scala
@@ -12,21 +12,24 @@ object Day6 {
   def main(args: Array[String]): Unit = {
     val input = readResourceLines("day6.txt")
 
-    val times = input.head.split(" ").filter(_.nonEmpty).drop(1).map(_.toInt)
-    val distances = input.last.split(" ").filter(_.nonEmpty).drop(1).map(_.toInt)
+    val times = input.head.split(" ").filter(_.nonEmpty).drop(1).map(_.toLong)
+    val distances = input.last.split(" ").filter(_.nonEmpty).drop(1).map(_.toLong)
+
+    val timeTotalRace = input.head.stripPrefix("Time: ").replace(" ", "").toLong
+    val distanceTotalRace = input.last.stripPrefix("Distance: ").replace(" ", "").toLong
 
     val part1 = times.zip(distances).map {
       case (time, distanceToBeat) => numWaysToBeatRace(time, distanceToBeat)
     }.product
-    val part2 = 0
+    val part2 = numWaysToBeatRace(timeTotalRace, distanceTotalRace)
 
     println(s"Part 1: $part1")
     println(s"Part 2: $part2")
   }
 
   @tailrec
-  def numWaysToBeatRace(raceTime: Int, distanceToBeat: Int, initialHold: Int = 1, numWaysAcc: Int = 0): Int = if (initialHold == raceTime) numWaysAcc else {
+  def numWaysToBeatRace(raceTime: Long, distanceToBeat: Long, initialHold: Long = 1L, numWaysAcc: Long = 0L): Long = if (initialHold == raceTime) numWaysAcc else {
     val myDistance = (raceTime - initialHold) * initialHold
-    numWaysToBeatRace(raceTime, distanceToBeat, initialHold + 1, numWaysAcc + (if (myDistance > distanceToBeat) 1 else 0))
+    numWaysToBeatRace(raceTime, distanceToBeat, initialHold + 1, numWaysAcc + (if (myDistance > distanceToBeat) 1L else 0L))
   }
 }
