31b8a0890e24c779f07f500318f4c70b3ec475a8
xRuiAlves <ruialves.esrt.98@gmail.com>
2023-12-08 07:35:54 +0000

    Add helper function to compute LCM in array of numbers

--- util/MathUtils.scala
+++ util/MathUtils.scala
@@ -6,4 +6,9 @@ object MathUtils {
     else gcd(n2, n1 % n2)
 
   def lcm(n1: Long, n2: Long): Long = Math.abs(n1 * n2) / gcd(n1, n2)
+
+  def lcm(numbers: Array[Long]): Long = {
+    require(numbers.length >= 2, "At least 2 numbers required to calculate the least common multiple!!")
+    numbers.reduce((a, b) => lcm(a, b))
+  }
 }
