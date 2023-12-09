d811650b6cbd62d401a9d064fdbc676b145677f6
xRuiAlves <ruialves.esrt.98@gmail.com>
2023-12-09 07:50:22 +0000

    Improve day 9 solution

--- Day9.scala
+++ Day9.scala
@@ -5,37 +5,24 @@
 
 import util.ResourceUtils.readResourceLines
 
-import scala.annotation.tailrec
-
 object Day9 {
   def main(args: Array[String]): Unit = {
-    val input = readResourceLines("day9.txt")
-      .map(strToIntArr)
-
-    val part1 = input
+    val extrapolatedValues = readResourceLines("day9.txt")
+      .map(_.split(" ").map(_.toInt))
       .map(findExtrapolatedValue)
-      .map(_.last)
-      .sum
-    val part2 = input
-      .map(findExtrapolatedValue2)
-      .map(_.head)
-      .sum
+
+    val part1 = extrapolatedValues.map(_.last).sum
+    val part2 = extrapolatedValues.map(_.head).sum
 
     println(s"Part 1: $part1")
     println(s"Part 2: $part2")
   }
 
-  def strToIntArr(str: String): Array[Int] = str.split(" ").map(_.toInt)
-
-  def findExtrapolatedValue(sequence: Array[Int]): Array[Int] = if (sequence.forall(_ == 0)) sequence.appended(0) else {
-    val diffs = sequence.sliding(2).map(pair => pair.last - pair.head).toArray
-    val diff = findExtrapolatedValue(diffs).last
-    sequence.appended(sequence.last + diff)
-  }
-
-  def findExtrapolatedValue2(sequence: Array[Int]): Array[Int] = if (sequence.forall(_ == 0)) sequence.appended(0) else {
-    val diffs = sequence.sliding(2).map(pair => pair.last - pair.head).toArray
-    val diff = findExtrapolatedValue2(diffs).head
-    Array(sequence.head - diff).appendedAll(sequence)
-  }
+  def findExtrapolatedValue(sequence: Array[Int]): Array[Int] =
+    if (sequence.forall(_ == 0)) 0 +: sequence :+ 0
+    else {
+      val diffs = sequence.sliding(2).map(pair => pair.last - pair.head).toArray
+      val next = findExtrapolatedValue(diffs)
+      (sequence.head - next.head) +: sequence :+ (sequence.last + next.last)
+    }
 }
