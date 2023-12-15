8d11e2eec8d6dabc919069c58be6f4c82c0a5f1d
xRuiAlves <ruialves.esrt.98@gmail.com>
2023-12-15 08:45:29 +0000

    Add day 15 part 2

--- Day15.scala
+++ Day15.scala
@@ -12,18 +12,55 @@ object Day15 {
   private[this] final val ASCII_MOD_SPACE = 256
   private[this] final val ASCII_MULTIPLIER = 17
 
+  case class Instruction(private val instructionStr: String) {
+    val label: String = instructionStr.takeWhile(_.isLetter)
+    val hash: Int = getHash(label)
+
+    val focalLength: Int = instructionStr.filter(_.isDigit) match
+      case focalLengthStr if focalLengthStr.nonEmpty => focalLengthStr.toInt
+      case _ => -1
+  }
+
+  case class Box(label: String, focalLength: Int)
+
   def main(args: Array[String]): Unit = {
     val input = readResourceLine("day15.txt")
     val words = input.split(",")
+    val instructions = words.map(word => Instruction(word))
 
     val part1 = words.map(getHash).sum
-    val part2 = 0
+    val part2 = getBoxes(instructions)
+      .zipWithIndex
+      .map { case (box, boxIdx) => getBoxValue(box, boxIdx) }
+      .sum
 
     println(s"Part 1: $part1")
     println(s"Part 2: $part2")
   }
 
+  def getBoxes(instructions: Array[Instruction]): Array[Array[Box]] = {
+    val boxes = (0 until ASCII_MOD_SPACE).map(_ => mutable.ArrayBuffer[Box]()).toArray
+
+    instructions.foreach(instruction => {
+      val idx = boxes(instruction.hash).indexWhere(_.label == instruction.label)
+      if (instruction.focalLength == -1) {
+        if (idx != -1) boxes(instruction.hash).remove(idx)
+      } else {
+        val node = Box(instruction.label, instruction.focalLength)
+        if (idx != -1) boxes(instruction.hash)(idx) = node
+        else boxes(instruction.hash).addOne(node)
+      }
+    })
+
+    boxes.map(_.toArray)
+  }
+
   def getHash(word: String): Int = word
     .toCharArray
     .foldLeft(0)((acc, curr) => (acc + curr) * ASCII_MULTIPLIER % ASCII_MOD_SPACE)
+
+  def getBoxValue(box: Array[Box], boxIdx: Int): Int = box
+    .zipWithIndex
+    .map { case (boxNode, boxNodeIdx) => (boxIdx + 1) * (boxNodeIdx + 1) * boxNode.focalLength }
+    .sum
 }
