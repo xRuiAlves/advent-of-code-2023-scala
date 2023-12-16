37fdf5bd4c25f0a80f7b110de7cc682c3ede9487
xRuiAlves <ruialves.esrt.98@gmail.com>
2023-12-16 08:00:53 +0000

    Fix day 16 formatting

--- Day16.scala
+++ Day16.scala
@@ -54,7 +54,7 @@ object Day16 {
   def isInBounds(map: Mat2D, i: Int, j: Int): Boolean =
     i >= 0 && j >= 0 && i < map.length && j < map(i).length
 
-  def applyStep(node: Node): Node = node match
+  def applyStepForward(node: Node): Node = node match
     case ((i, j), 'R') => ((i, j + 1), 'R')
     case ((i, j), 'L') => ((i, j - 1), 'L')
     case ((i, j), 'U') => ((i - 1, j), 'U')
@@ -64,35 +64,37 @@ object Day16 {
     val cell = map(node._1._1)(node._1._2)
     val ((i, j), dir) = node
 
-    if (cell == '.') Array(applyStep(node))
+    if (cell == '.') Array(applyStepForward(node))
     else if (cell == '-') {
-      if (dir == 'R' || dir == 'L') Array(applyStep(node))
-      else Array(
-        ((i, j - 1), 'L'),
-        ((i, j + 1), 'R')
+      if (dir == 'R' || dir == 'L') Array(applyStepForward(node))
+      else
+        Array(
+          ((i, j - 1), 'L'),
+          ((i, j + 1), 'R')
+        )
+    } else if (cell == '|') {
+      if (dir == 'U' || dir == 'D') Array(applyStepForward(node))
+      else
+        Array(
+          ((i - 1, j), 'U'),
+          ((i + 1, j), 'D')
+        )
+    } else if (cell == '\\')
+      Array(
+        if (dir == 'R') ((i + 1, j), 'D')
+        else if (dir == 'L') ((i - 1, j), 'U')
+        else if (dir == 'U') ((i, j - 1), 'L')
+        else if (dir == 'D') ((i, j + 1), 'R')
+        else throw new Error(s"Invalid dir '$dir' at node $node")
       )
-    }
-    else if (cell == '|') {
-      if (dir == 'U' || dir == 'D') Array(applyStep(node))
-      else Array(
-        ((i - 1, j), 'U'),
-        ((i + 1, j), 'D')
+    else if (cell == '/')
+      Array(
+        if (dir == 'R') ((i - 1, j), 'U')
+        else if (dir == 'L') ((i + 1, j), 'D')
+        else if (dir == 'U') ((i, j + 1), 'R')
+        else if (dir == 'D') ((i, j - 1), 'L')
+        else throw new Error(s"Invalid dir '$dir' at node $node")
       )
-    }
-    else if (cell == '\\') Array(
-      if (dir == 'R') ((i + 1, j), 'D')
-      else if (dir == 'L') ((i - 1, j), 'U')
-      else if (dir == 'U') ((i, j - 1), 'L')
-      else if (dir == 'D') ((i, j + 1), 'R')
-      else throw new Error(s"Invalid dir '$dir' at node $node")
-    )
-    else if (cell == '/') Array(
-      if (dir == 'R') ((i - 1, j), 'U')
-      else if (dir == 'L') ((i + 1, j), 'D')
-      else if (dir == 'U') ((i, j + 1), 'R')
-      else if (dir == 'D') ((i, j - 1), 'L')
-      else throw new Error(s"Invalid dir '$dir' at node $node")
-    )
     else throw new Error(s"Invalid cell value '$cell' at node $node")
   }
 }
