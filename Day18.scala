341d3545eb3f299f71761651f84f7f8444aa86b5
xRuiAlves <ruialves.esrt.98@gmail.com>
2023-12-18 07:42:00 +0000

    Improve day 18 part 1

--- Day18.scala
+++ Day18.scala
@@ -11,27 +11,32 @@ object Day18 {
   type Coord2D = (Int, Int)
   def main(args: Array[String]): Unit = {
     val input = readResourceLines("day18.txt")
-    val instructions = parseInstructions(input)
-    val paintedSquares = getPaintedSquares(instructions)
+    val instructions = parsePaintedInstructions(input)
+    val polygonPart1 = Polygon(getPaintedSquares(instructions.map(_.instruction)))
 
-    var area = 0
-    paintedSquares.sliding(2).foreach { case Array((x1, y1), (x2, y2)) =>
-      area += x1 * y2 - x2 * y1
-    }
-    val perimeter = paintedSquares.length
-    val interiorArea = math.abs(area / 2) - perimeter / 2
-
-    val part1 = interiorArea + perimeter
+    val part1 = polygonPart1.area
     val part2 = 0
 
     println(s"Part 1: $part1")
     println(s"Part 2: $part2")
   }
 
-  case class Instruction(dir: Char, dist: Int, color: String)
+  case class Polygon(points: Array[Coord2D]) {
+    private val shoelaceSum = points.sliding(2).foldLeft(0) {
+      case (sum, Array((x1, y1), (x2, y2))) => sum + x1 * y2 - x2 * y1
+    }
+    val perimeter: Int = points.length
 
-  def parseInstructions(rawInstructions: Array[String]): Array[Instruction] = rawInstructions.map {
-    case s"$dir $dist ($color)" => Instruction(dir.head, dist.toInt, color)
+    private val interiorArea = math.abs(shoelaceSum / 2) - perimeter / 2
+    val area: Int = interiorArea + perimeter
+  }
+
+  case class Instruction(dir: Char, dist: Int)
+
+  case class ColorInstruction(instruction: Instruction, color: String)
+
+  def parsePaintedInstructions(rawInstructions: Array[String]): Array[ColorInstruction] = rawInstructions.map {
+    case s"$dir $dist ($color)" => ColorInstruction(Instruction(dir.head, dist.toInt), color)
   }
 
   def getPaintedSquares(instructions: Array[Instruction]): Array[Coord2D] = {
@@ -39,7 +44,7 @@ object Day18 {
     var curr = (0, 0)
     visited.addOne(curr)
 
-    instructions.foreach { case Instruction(dir, dist, _) =>
+    instructions.foreach { case Instruction(dir, dist) =>
       for (_ <- 0 until dist) {
         curr = (curr, dir) match {
           case ((i, j), 'R') => (i, j + 1)
