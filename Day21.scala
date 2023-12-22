33d7599de00af50d79c3d0816d33a753cda4fb95
xRuiAlves <ruialves.esrt.98@gmail.com>
2023-12-22 08:06:33 +0000

    Add day 21 part 2

--- Day21.scala
+++ Day21.scala
@@ -15,63 +15,84 @@ object Day21 {
 
   private[this] final val Start = 'S'
   private[this] final val Garden = '.'
-  private[this] final val Rock = '#'
-  private[this] final val NumSteps = 64
+  private[this] final val NumStepsPart1 = 64
+  private[this] final val NumStepsPart2 = 26501365
 
   def main(args: Array[String]): Unit = {
     val input = readResourceLines("day21.txt")
     val map = input.map(_.toCharArray)
-    val start = getMapStart(map)
+    val gardens = getGardens(map)
+    val start = (map.length / 2, map.length / 2)
 
-    val part1 = twoStepVisit(map, start, NumSteps)
-    val part2 = 0
+    val interpolationIndexes = getInterpolationIndexes(map.length)
+    val visited = visit(gardens, map.length, start, interpolationIndexes.last)
+
+    val part1 = visited.count(_.depth == NumStepsPart1)
+    val part2 = interpolation(visited, map.length, interpolationIndexes, NumStepsPart2)
 
     println(s"Part 1: $part1")
     println(s"Part 2: $part2")
   }
 
-  def getMapStart(map: Map): Coord2D = {
-    for (i <- map.indices; j <- map(i).indices) if (map(i)(j) == Start) {
-      map(i)(j) = Garden
-      return (i, j)
+  def getGardens(map: Map): Set[Coord2D] = {
+    val gardens = mutable.Set[Coord2D]()
+
+    for (i <- map.indices; j <- map(i).indices) {
+      if (map(i)(j) == Start) {
+        map(i)(j) = Garden
+      }
+      if (map(i)(j) == Garden) {
+        gardens.addOne((i, j))
+      }
     }
-    throw new Error("Map start position not found!")
+
+    gardens.toSet
   }
 
-  def isInBounds(map: Map, i: Int, j: Int): Boolean =
-    i >= 0 && j >= 0 && i < map.length && j < map(i).length
-
-  def getGardenNeighbors(map: Map, coord: Coord2D): Set[Coord2D] =
-    getGardenNeighbors(map, coord._1, coord._2)
-  def getGardenNeighbors(map: Map, i: Int, j: Int): Set[Coord2D] = Set(
+  def getGardenNeighbors(gardens: Set[Coord2D], mapSize: Int, coord: Coord2D): Set[Coord2D] =
+    getGardenNeighbors(gardens, mapSize, coord._1, coord._2)
+  def getGardenNeighbors(gardens: Set[Coord2D], mapSize: Int, i: Int, j: Int): Set[Coord2D] = Set(
     (i, j - 1),
     (i, j + 1),
     (i - 1, j),
     (i + 1, j)
   ).filter { case (ni, nj) =>
-    isInBounds(map, ni, nj) && map(ni)(nj) == Garden
+    gardens.contains((Math.floorMod(ni, mapSize), Math.floorMod(nj, mapSize)))
   }
 
-  def getGardenTwoStepNeighbors(map: Map, coord: Coord2D): Set[Coord2D] =
-    getGardenNeighbors(map, coord).flatMap(neighbor => getGardenNeighbors(map, neighbor))
+  case class VisitNode(coord: Coord2D, depth: Int)
 
-  case class SearchNode(coord: Coord2D, depth: Int)
-
-  def twoStepVisit(map: Map, start: Coord2D, targetDepth: Int): Int = {
-    val visited = mutable.Set[Coord2D]()
-    val toVisit = mutable.Queue[SearchNode]()
-    toVisit.enqueue(SearchNode(start, 0))
+  def visit(gardens: Set[Coord2D], mapSize: Int, start: Coord2D, targetDepth: Int): Set[VisitNode] = {
+    val toVisit = mutable.Queue[VisitNode]()
+    val visited = mutable.Set[VisitNode]()
+    toVisit.enqueue(VisitNode(start, 0))
 
     while (toVisit.nonEmpty) {
-      val SearchNode(coord, depth) = toVisit.dequeue()
+      val curr = toVisit.dequeue()
 
-      if (!visited.contains(coord) && depth <= targetDepth) {
-        visited.addOne(coord)
-        getGardenTwoStepNeighbors(map, coord).foreach(neighbor => toVisit.enqueue(SearchNode(neighbor, depth + 2)))
+      if (!visited.contains(curr) && curr.depth <= targetDepth) {
+        visited.addOne(curr)
+        getGardenNeighbors(gardens, mapSize, curr.coord).foreach(neighbor => toVisit.enqueue(VisitNode(neighbor, curr.depth + 1)))
       }
     }
 
-    visited.size
+    visited.toSet
   }
 
+  def getInterpolationIndexes(mapSize: Int): IndexedSeq[Int] = for (i <- 0 to 2) yield mapSize / 2 + mapSize * i
+
+  // https://en.wikipedia.org/wiki/Polynomial_interpolation
+  // I'm a bit at loss at why this works, but after way too many off-by-(a few) errors, I managed to get the right value
+  // Got some inspiration from Reddit in regards to the use of the polynomial interplation, but I still think there's something
+  // off with my formula, although I can't put my finger on what it might be 🤷
+  def interpolation(visited: Set[VisitNode], mapSize: Int, interpolationIndexes: IndexedSeq[Int], numSteps: Int): Long = {
+    val interpolationPoints = interpolationIndexes.map(i => visited.count(_.depth == i).toLong)
+
+    val a = interpolationPoints(0)
+    val b = interpolationPoints(1) - interpolationPoints(0)
+    val c = interpolationPoints(2) - interpolationPoints(1)
+    val numInterpolationExpansions = numSteps / mapSize
+
+    a + b * numInterpolationExpansions + (numInterpolationExpansions * (numInterpolationExpansions - 1L) / 2L) * (c - b)
+  }
 }
